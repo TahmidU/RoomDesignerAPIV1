6539a7cefafc4d744c4d51b89d922db4319632d8
TahmidU <tahmiduddin@hotmail.co.uk>
2019-11-03 13:51:31 +0000

    Initial commit. Setup dependencies.

---
+++ src/main/java/com/aarrd/room_designer/RoomDesignerApplication.java
@@ -0,0 +1,13 @@
+package com.aarrd.room_designer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomDesignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomDesignerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aarrd/room_designer/RoomDesignerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarrd.room_designer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomDesignerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
